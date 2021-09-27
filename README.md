7bf38273c6d6d81ecfe9ff3ae0d6ca0581a6f29f
Sgobbi Federico <federico@sgobbi.it>
2021-09-27 22:48:46 +0200

    First release

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nodejs server util pick
+Nodejs common server util pick.
+
+### Install
+```
+npm install @esultocom/node-server-util-pick
+```
---
+++ src/index.js
@@ -0,0 +1,23 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                 *
+ *    Copyright (c) 2021 Sgobbi Federico                                                           *
+ *    All rights reserved.                                                                         *
+ *                                                                                                 *
+ *    This file is licensed under the MIT License.                                                 *
+ *    License text available at https://opensource.org/licenses/MIT                                *
+ *                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// * * * * * * * * The code
+const pick = (object, keys) => {
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      // eslint-disable-next-line no-param-reassign
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
+};
+
+// * * * * * * * * Module export
+module.exports = pick;
